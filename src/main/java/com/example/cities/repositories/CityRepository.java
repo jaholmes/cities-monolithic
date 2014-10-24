ef170b4eeed1e2f78844ed2fc01494bfcc1e6f6e
Jeff Holmes <jeffaholmes@gmail.com>
2014-10-23 23:08:50 -0700

    Added resource assembler to fix paging

--- src/main/java/com/example/cities/repositories/CityRepository.java
+++ src/main/java/com/example/cities/repositories/CityRepository.java
@@ -1,9 +1,11 @@
 package com.example.cities.repositories;
 
 import com.example.cities.domain.City;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 
 public interface CityRepository extends PagingAndSortingRepository<City, Long> {
@@ -12,11 +14,11 @@ public interface CityRepository extends PagingAndSortingRepository<City, Long> {
 
     City findById(Long id);
 
-    Page<City> findByNameIgnoreCase(String name, Pageable pageable);
+    Page<City> findByNameIgnoreCase(@Param("q") String name, Pageable pageable);
 
-    Page<City> findByNameContainsIgnoreCase(String name, Pageable pageable);
+    Page<City> findByNameContainsIgnoreCase(@Param("q") String name, Pageable pageable);
 
-    Page<City> findByStateCodeIgnoreCase(String stateCode, Pageable pageable);
+    Page<City> findByStateCodeIgnoreCase(@Param("q") String stateCode, Pageable pageable);
 
-    Page<City> findByPostalCode(String postalCode, Pageable pageable);
+    Page<City> findByPostalCode(@Param("q") String postalCode, Pageable pageable);
 }

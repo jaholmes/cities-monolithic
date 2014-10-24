874f25d97429193fe0f67d4dc73fa0046e463673
jholmes-pivotal <jholmes@pivotal.io>
2014-10-24 09:34:53 -0700

    Added EnableJpaRepositories

--- src/main/java/com/example/cities/Application.java
+++ src/main/java/com/example/cities/Application.java
@@ -16,7 +16,7 @@ import com.example.cities.resources.CityResourceAssembler;
 @ComponentScan
 @EnableAutoConfiguration
 @EnableSpringDataWebSupport
-//@EnableJpaRepositories
+@EnableJpaRepositories
 public class Application {
 
     public static void main(String[] args) {
